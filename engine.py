72207a8ff1c58559ea3ce16f701108fa2429ff51
Benjamin Lee <ben@mineturtles.com>
2020-05-27 22:14:59 -0700

    Added bids sheet generation function

--- engine.py
+++ engine.py
@@ -32,14 +32,14 @@ def create_summary_sheet(users_df, demands_df):
     summary_player_headers = ([f(i) for i in user_ids for f in (header_revenue, header_cost, header_profit)] 
                                 + [header_balance(i) for i in user_ids])
 
-    # Note that this is a local variable. As summary_csv will constantly be written and read from summary.csv, it is
+    # Note that this is a local variable. As summary_df will constantly be written and read from summary.csv, it is
     # important to load from the True summary.csv file whenever the data is needed, instead of a potentially out-of-
-    # date summary_csv variable. Whenever possible, functions should interact indirectly through writing to summary.csv
+    # date summary_df variable. Whenever possible, functions should interact indirectly through writing to summary.csv
     # and then reading summary.csv, instead of chaining functions directly.
-    summary_csv = pd.concat([demands_df, pd.DataFrame(columns=(['price'] + summary_player_headers))], axis=1)
+    summary_df = pd.concat([demands_df, pd.DataFrame(columns=(['price'] + summary_player_headers))], axis=1)
 
     # summary.csv is saved in the /csv/ directory.
-    summary_csv.to_csv('csv/summary.csv')
+    summary_df.to_csv('csv/summary.csv')
 
 # HOURLY SPREADSHEETS:
 # A set of spreadsheets (one per hour) recording bids, production, and revenue/cost for each unit. 
@@ -64,5 +64,40 @@ def create_hourly_sheets(demands_df, portfolios_df):
         # round_r_hour_h.csv is saved in the /csv/hourly/ directory.
         hourly_df.to_csv('csv/hourly/round_' + str(r) + '_hour_' + str(h) + '.csv')
 
+# CURRENT BID SPREADSHEET:
+# Records bids for all hours as submitted by players. Constantly updating according to player form 
+# inputs. Not visible to players, only administrators. When an hour is run, the bids in this 
+# spreadsheet are recorded in the corresponding round_#_hour_#.csv file.
+# -   bids : portfolio_id,portfolio_name,unit_id,unit_name, 
+#     [bid_base_{round}_{hour},bid_up_{round}_{hour},bid_down_{round}_{hour}] 
+#     : The square brackets indicate that these columns are to be repeated dynamically based on the 
+#     number of rounds and hours. Note that this structure enables unit-specific adjustment bids; 
+#     whether or not players can specify those values is at the discretion of the bid form creator. 
+
+def create_bids_sheet(portfolios_df, demands_df):
+    # a list of (round, hour) pairs, for the purpose of naming the repeated columns
+    round_hour_tuples = list(demands_df[['round', 'hour']].itertuples(index=False, name=None))
+
+    # the part of portfolios_df that is used to construct bids_df
+    portfolios_headers = ['portfolio_id','portfolio_name','unit_id','unit_name']
+
+    # lambda functions for the header-generating list comprehension
+    # each of these functions is equivalent to lambda (r, h): ... , but tuple unpacking was removed
+    header_base = lambda r_h: 'bid_base_' + str(r_h[0]) + '_' + str(r_h[1])
+    header_up   = lambda r_h: 'bid_up_'   + str(r_h[0]) + '_' + str(r_h[1])
+    header_down = lambda r_h: 'bid_down_' + str(r_h[0]) + '_' + str(r_h[1])
+
+    bids_r_h_headers = ([f(r_h) for r_h in round_hour_tuples for f in (header_base, header_up, header_down)])
+
+    bids_df = pd.concat([portfolios_df[portfolios_headers], pd.DataFrame(columns=bids_r_h_headers)], axis=1)
+
+    # bids.csv is saved in the /csv/ directory.
+    bids_df.to_csv('csv/bids.csv')
+
+
 create_summary_sheet(users_df, demands_df)
 create_hourly_sheets(demands_df, portfolios_df)
+create_bids_sheet(portfolios_df, demands_df)
+
+
+
