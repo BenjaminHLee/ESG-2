a6fc1a937d770a7804984fc19e90b8d0a51b5d56
Benjamin Lee <ben@mineturtles.com>
2020-05-27 21:53:39 -0700

    Replaced _csv with _df in variable names
    The variables are dataframes, so labeling them as _csv files is misleading

--- engine.py
+++ engine.py
@@ -8,9 +8,9 @@ import csv
 
 
 # read CONFIG SPREADSHEETS from /csv/config files
-demands_csv = pd.read_csv('csv/config/demands.csv')
-portfolios_csv = pd.read_csv('csv/config/portfolios.csv')
-users_csv = pd.read_csv('csv/config/users.csv')
+demands_df = pd.read_csv('csv/config/demands.csv')
+portfolios_df = pd.read_csv('csv/config/portfolios.csv')
+users_df = pd.read_csv('csv/config/users.csv')
 
 # SUMMARY SPREADSHEET:
 # Captures a macroscopic summary of prices over hours.
@@ -18,8 +18,8 @@ users_csv = pd.read_csv('csv/config/users.csv')
 #     [player_{player_id}_revenue,player_{player_id}_cost,player_{player_id}_profit],
 #     [player_{player_id}_balance] 
 
-def create_summary_sheet(users_csv, demands_csv):
-    user_ids = users_csv['portfolio_id'].tolist()
+def create_summary_sheet(users_df, demands_df):
+    user_ids = users_df['portfolio_id'].tolist()
 
     # lambda functions for the header-generating list comprehension
     header_revenue  = lambda i: 'player_' + str(i) + '_revenue'
@@ -36,7 +36,7 @@ def create_summary_sheet(users_csv, demands_csv):
     # important to load from the True summary.csv file whenever the data is needed, instead of a potentially out-of-
     # date summary_csv variable. Whenever possible, functions should interact indirectly through writing to summary.csv
     # and then reading summary.csv, instead of chaining functions directly.
-    summary_csv = pd.concat([demands_csv, pd.DataFrame(columns=(['price'] + summary_player_headers))], axis=1)
+    summary_csv = pd.concat([demands_df, pd.DataFrame(columns=(['price'] + summary_player_headers))], axis=1)
 
     # summary.csv is saved in the /csv/ directory.
     summary_csv.to_csv('csv/summary.csv')
@@ -50,19 +50,19 @@ def create_summary_sheet(users_csv, demands_csv):
 #     activated,mwh_produced,mwh_adjusted_down,carbon_produced,price, 
 #     revenue,adjust_down_revenue,cost_var,cost_om,profit 
 
-def create_hourly_sheets(demands_csv, portfolios_csv):
+def create_hourly_sheets(demands_df, portfolios_df):
     # a list of (round, hour) pairs, for the purpose of naming each sheet
-    round_hour_tuples = list(demands_csv[['round', 'hour']].itertuples(index=False, name=None))
+    round_hour_tuples = list(demands_df[['round', 'hour']].itertuples(index=False, name=None))
 
     hourly_additional_headers = ['bid_base','bid_up','bid_down','activated','mwh_produced',
                                  'mwh_adjusted_down','carbon_produced','price','revenue',
                                  'adjust_down_revenue','cost_var','cost_om','profit']
 
     for (r, h) in round_hour_tuples:
-        hourly_csv = pd.concat([portfolios_csv, pd.DataFrame(columns=(hourly_additional_headers))], axis=1)
+        hourly_df = pd.concat([portfolios_df, pd.DataFrame(columns=(hourly_additional_headers))], axis=1)
 
         # round_r_hour_h.csv is saved in the /csv/hourly/ directory.
-        hourly_csv.to_csv('csv/hourly/round_' + str(r) + '_hour_' + str(h) + '.csv')
+        hourly_df.to_csv('csv/hourly/round_' + str(r) + '_hour_' + str(h) + '.csv')
 
-create_summary_sheet(users_csv, demands_csv)
-create_hourly_sheets(demands_csv, portfolios_csv)
+create_summary_sheet(users_df, demands_df)
+create_hourly_sheets(demands_df, portfolios_df)
